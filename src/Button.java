7f6c48e533b6c06683eea2d941b13ab49a7dc414
localmgr <xiaj@rose-hulman.edu>
2021-04-18 17:44:51 -0400

    partially implemented variables, getters and setters for Button, Task and Label class. One argument found in Label class: Should we reset all labels each time, or add and delete one by one?

--- src/Button.java
+++ src/Button.java
@@ -1,4 +1,35 @@
+import java.awt.Point;
+import java.awt.geom.Point2D;
 
-public class Button {
+import javax.swing.JButton;
 
+public abstract class Button extends JButton{
+	//TODO: implement appearance, link
+	protected String text; 
+	protected Point upperLeftPosition;
+	//protected ??? appearance
+	//protected ??? link 
+	
+	//Getters, setters and variables for size and text are implemented by JButton. Size is in Dimension, and text is in String
+	
+	//setters 
+	public void setAppearance(/*appearance*/) {
+		
+	}
+	
+	public void setPosition(Point pt) {
+		this.upperLeftPosition = pt; 
+	}
+	
+	public void setPosition(double x, double y) {
+		this.upperLeftPosition = new Point((int)x,(int)y); 
+	}
+	
+	//getters 
+	//public ??? getAppearance(){}
+	
+	public Point getPosition() {
+		return this.upperLeftPosition; 
+	}
+	
 }
