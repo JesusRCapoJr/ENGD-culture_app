358fd7c281184156c7a1a656ed9071d8a01cab30
localmgr <xiaj@rose-hulman.edu>
2021-04-19 10:50:37 -0400

    Updated Folder and AddTaskPanel class. Folder class has a getTasksByLabels() method.

--- src/AddTaskPanel.java
+++ src/AddTaskPanel.java
@@ -1,4 +1,20 @@
+import java.awt.Panel;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class AddTaskPanel {
-
+	private JPanel gui; 
+	private double windowSizeX; 
+	private double windowSizeY; 
+	
+	public AddTaskPanel(double windowSizeX, double windowSizeY, JFrame world) {
+		this.gui = new JPanel(); 
+		world.add(gui); 
+		this.windowSizeX = windowSizeX; 
+		this.windowSizeY = windowSizeY; 
+		
+		//construct the whole window
+	}
+	
 }
--- src/Folder.java
+++ src/Folder.java
@@ -1,4 +1,42 @@
+import java.util.ArrayList;
 
 public class Folder {
-
+	private ArrayList<Task> tasks;
+	
+	public Folder() {
+		tasks = new ArrayList<Task>(); 
+	}
+	
+	public void addTask(Task task) {
+		this.tasks.add(task); 
+	}
+	
+	public ArrayList<Task> getTasks() {
+		return this.tasks; 
+	}
+	
+	/**
+	 * Searches for and returns Task objects within this Folder with all Label objects specified. O(n)
+	 * TODO: test it
+	 * 
+	 * @param labels: ArrayList<Label>
+	 * @return tasksFound: tasks containing all labels. Note they may have more labels than specified. 
+	 */
+	public ArrayList<Task> getTasksByLabels(ArrayList<Label> labels){
+		ArrayList<Task> tasksFound = new ArrayList<Task>(); 
+		for(Task i: tasks) {
+			ArrayList<Label> labelsInTask = i.getLabels();  
+			boolean matches = true; 
+			for(Label ii: labels) {
+				if(!labelsInTask.contains(ii)) {
+					matches = false; 
+				}
+			}
+			if(matches) {
+				tasksFound.add(i); 
+			}
+		}
+		
+		return tasksFound; 
+	}
 }
