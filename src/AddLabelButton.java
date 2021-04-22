b155a13e7b29f2691d33d442f3effa4d5d58b729
mcclunk <mcclunk@rose-hulman.edu>
2021-04-22 11:24:56 -0400

    AddLabel Update

--- src/AddLabelButton.java
+++ src/AddLabelButton.java
@@ -1,4 +1,19 @@
-
 public class AddLabelButton {
+	import java.awt.Color;
+	import java.swing.JPanel;
+	import java.awt.event.ActionEvent;
+	import java.awt.event.ActionListener;
+
+	public class AddLabelButton extends Button {
+		private JPanel world;
+		public static final double ADD_TASK_PANEL_X_RATIO = 0.3;
+		public static final double ADD_TASK_PANEL_Y_RATIO = 0.5;  
+		private Color BGC = new Color(255,0,0,0); 
+
+		public AddLabelButton(JPanel labelOverview, double upperX, double upperY, double width, double height) {
+			
+		}
+		
+		this.addActionListener()
 
 }
