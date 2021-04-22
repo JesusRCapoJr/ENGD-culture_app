f71d0e87045d099a1000828bd5d919595a4d8613
localmgr <xiaj@rose-hulman.edu>
2021-04-22 10:05:41 -0400

    LABEL

---
+++ src/Label.java
@@ -0,0 +1,29 @@
+
+
+import java.awt.Color;
+
+public class Label {
+	//TODO: implement shape. 
+	private String text; 
+	private Color color; 
+	//private ??? shape; 
+	
+	public Label(String text, Color color/*, ??? shape*/) {
+		this.text = text; 
+		this.color = color; 
+		//this.shape = shape; 
+	}
+	
+	//setters 
+	public void setText(String newText) {
+		this.text = newText; 
+	}
+	
+	public void setColor(Color newColor) {
+		this.color = newColor; 
+	}
+	
+	/*public void setShape(??? newShape) {
+		this.shape = newShape; 
+	}*/ 
+}
