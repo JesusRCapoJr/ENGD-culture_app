7ee8b3dca805e0579abc9c4fb58da1264ac79e37
Jesus Capo <jcapostem@gmail.com>
2021-04-22 10:22:16 -0400

    Main updates

--- src/Main.java
+++ src/Main.java
@@ -17,13 +17,10 @@ import javax.swing.JPanel;
  * This app is created by Nathan Atkinson, Jesus Capo, John Chung, Steven Xia, and Kieya McClung.
  */
 public class Main {
-//	private static final int NUMBER_OF_SIMULATIONS = 2;
-//	private static final int MIN_WIDTH = 200;
-//	private static final int MAX_WIDTH = 600;
-//	private static final int MIN_HEIGHT = 100;
-//	private static final int MAX_HEIGHT = 300;
-//	private static final Color[] BACKGROUND_COLORS = { Color.RED,
-//			Color.BLUE, Color.GREEN };
+	private static final int WIDTH = 1920;
+	private static final int HEIGHT = 300;
+	private static final Color[] BACKGROUND_COLORS = { Color.RED,
+			Color.BLUE, Color.GREEN };
 
 	/**
 	 * Starts the application.
@@ -31,42 +28,12 @@ public class Main {
 	 * @param args
 	 *            ignored
 	 */
-	public static void main(String[] args) {
-		//ArrayList<SimulationPanel> worlds = constructSimulations();
-		//JFrame frame = new BallWorldsFrame(worlds);
-		//frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//frame.setVisible(true);
-		
+	public static void main(String[] args) {		
 		JFrame pandativityFrame = new PandativityFrame();
 		pandativityFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		pandativityFrame.setSize(new Dimension(1920,1080));
-		
-//		PandativityFrameUpdater updater = new PandativityFrameUpdater(pandativityFrame);
-//		updater. Test
-		JPanel navigationPanel = new JPanel();
-//		JButton testButton = new JButton("Words.");
-//		navigationPanel.add(testButton);
-//		pandativityFrame.add(navigationPanel, BorderLayout.NORTH);
-//		pandativityFrame.setVisible(true);
-		
+		pandativityFrame.setSize(new Dimension(WIDTH,HEIGHT));
+		JPanel navigationPanel = new JPanel();		
 	}
 
-	/**
-	 * This helper method constructs a list of simulation objects that will each
-	 * simulate a world of bouncing balls.
-	 * 
-	 * @return a list of worlds
-	 */
-//	private static ArrayList<SimulationPanel> constructSimulations() {
-//		ArrayList<SimulationPanel> result = new ArrayList<SimulationPanel>();
-//		for (int i = 0; i < NUMBER_OF_SIMULATIONS; i++) {
-//			int width = Random.randRange(MIN_WIDTH, MAX_WIDTH);
-//			int height = Random.randRange(MIN_HEIGHT, MAX_HEIGHT);
-//			Color c = BACKGROUND_COLORS[i % BACKGROUND_COLORS.length];
-//			SimulationPanel sp = new SimulationPanel(width, height, c);
-//			result.add(sp);
-//		}
-//		return result;
-//	}
 
 }
