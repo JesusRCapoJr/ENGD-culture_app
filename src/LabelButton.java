23668e27b55b146597851d4c3008817adfbd1114
atkinsnp <Atkinsnp@rose-hulman.edu>
2021-04-22 10:07:25 -0400

    push

--- src/LabelButton.java
+++ src/LabelButton.java
@@ -1,4 +1,15 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class LabelButton {
-
+public class LabelButton extends Button {
+	
+	public void createAction() {                  // Action
+		ActionListener al = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+//				System.out.println("Put actions to be preformed on click here");		// what it does goes here
+				
+				}
+			};
+	}
+	
 }
