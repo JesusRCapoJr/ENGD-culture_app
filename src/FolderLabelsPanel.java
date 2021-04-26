7bce6cbc298babbeed580f2cba73022c7476434a
mcclunk <mcclunk@rose-hulman.edu>
2021-04-26 10:39:49 -0400

    Add general code to folderlabelpanel

--- src/FolderLabelsPanel.java
+++ src/FolderLabelsPanel.java
@@ -1,4 +1,13 @@
 
-public class FolderLabelsPanel {
+import java.util.ArrayList;
+import javax.swing.JPanel;
 
+
+public class FolderLabelsPanel extends JPanel{
+private ArrayList<Label> labels = new ArrayList<Label>();
+
+//public void folderLabelsPanel() {
+// 
+//
+//}
 }
