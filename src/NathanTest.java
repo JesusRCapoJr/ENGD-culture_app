0bbfffc7b2110a2d967fe7e06e518b8ab34a6d01
nategreen28 <65096809+nategreen28@users.noreply.github.com>
2021-05-09 03:24:20 -0400

    Fixing the Bullshit

--- src/NathanTest.java
+++ src/NathanTest.java
@@ -11,6 +11,12 @@ import javax.swing.JMenuItem;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.IOException;
+import java.awt.Color;
+import javax.swing.JProgressBar;
+import java.awt.Component;
+import javax.swing.Box;
+import javax.swing.SwingConstants;
+import javax.swing.BoxLayout;
 
 public class NathanTest {
 
@@ -47,16 +53,19 @@ public class NathanTest {
 	 */
 	private void initialize() throws Throwable {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 1400, 800);
+		frame.getContentPane().setBackground(new Color(107, 142, 35));
+		frame.setBounds(0, 0, 1920, 1080);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JPanel OverviewPanel = new JPanel();
-		OverviewPanel.setBounds(701, 220, 675, 533);
+		final JPanel OverviewPanel = new JPanel();                    /// might not be finle
+		OverviewPanel.setBackground(new Color(105, 105, 105));
+		OverviewPanel.setBounds(724, 208, 806, 627);
 		frame.getContentPane().add(OverviewPanel);
 		
 		JPanel SpritePanel = new JPanel();
-		SpritePanel.setBounds(1037, 10, 339, 200);
+		SpritePanel.setBackground(new Color(255, 255, 255));
+		SpritePanel.setBounds(1028, 10, 241, 176);
 		//
 			SpriteForPanels sprite = new SpriteForPanels(frame,SpritePanel);
 			sprite.Score = 2;
@@ -65,39 +74,57 @@ public class NathanTest {
 		//
 		frame.getContentPane().add(SpritePanel);
 		
-		JButton btnNewButton = new JButton("openAnotherWindow");
-		btnNewButton.setBounds(10, 34, 150, 60);
-		frame.getContentPane().add(btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("openAnotherWindow");
-		btnNewButton_1.setBounds(185, 36, 151, 57);
-		frame.getContentPane().add(btnNewButton_1);
-		
-		JButton btnNewButton_2 = new JButton("openAnotherWindow");
-		btnNewButton_2.setBounds(365, 36, 151, 57);
-		frame.getContentPane().add(btnNewButton_2);
-		
-		JButton btnNewButton_3 = new JButton("Add New Folder");
-		btnNewButton_3.setBounds(541, 36, 150, 57);
-		frame.getContentPane().add(btnNewButton_3);
-		
 		JPanel AddTaskPanel = new JPanel();
+		AddTaskPanel.setBackground(new Color(112, 128, 144));
 		AddTaskPanel.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 //				TaskWindow.initialize();
 			}
 		});
-		AddTaskPanel.setBounds(10, 127, 681, 626);
+		AddTaskPanel.setBounds(10, 149, 704, 686);
 		frame.getContentPane().add(AddTaskPanel);
 		
-		JPanel TimeDatePanel = new JPanel();
-		TimeDatePanel.setBounds(701, 10, 326, 200);
-		frame.getContentPane().add(TimeDatePanel);
+		JProgressBar progressBar = new JProgressBar();
+		progressBar.setValue(50);
+		progressBar.setBackground(new Color(34, 139, 34));
+		progressBar.setBounds(1028, 183, 241, 14);
+		frame.getContentPane().add(progressBar);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(160, 82, 45));
+		panel.setBounds(10, 23, 704, 81);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Folder 1");
+		btnNewButton.setBounds(10, 10, 120, 60);
+		panel.add(btnNewButton);
+		
+		JButton btnNewButton_2 = new JButton("Folder 2");
+		btnNewButton_2.setBounds(200, 10, 120, 60);
+		panel.add(btnNewButton_2);
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		JButton btnNewButton_1 = new JButton("Folder 2");
+		btnNewButton_1.setBounds(386, 10, 120, 60);
+		panel.add(btnNewButton_1);
+		
+		JButton btnNewButton_3 = new JButton("Add New Folder");
+		btnNewButton_3.setBounds(574, 10, 120, 60);
+		panel.add(btnNewButton_3);
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				frame.setVisible(false);
-				NathanTest2.initialize();
+//				frame.setVisible(false);
+				try {
+					new DesplayState2(frame);
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 			}
 		});
 	}
