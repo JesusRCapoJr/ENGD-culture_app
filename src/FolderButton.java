e89f57ae031d895a1fa1153866ac4fee25369974
localmgr <xiaj@rose-hulman.edu>
2021-05-11 11:30:56 -0400

    There's gotta be some FolderViewPanel???

--- src/FolderButton.java
+++ src/FolderButton.java
@@ -1,4 +1,34 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class FolderButton {
+import javax.swing.JFrame;
+
+public class FolderButton extends Button {
+	private Folder folder;
+
+	public FolderButton(Folder folder) { //? 
+		super(); 
+		this.folder=folder;
+		this.setText(folder.getTitle());
+		this.createAction();
+	}
+	
+	public void createAction() {                  // Action
+		ActionListener al = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//System.out.println("Put actions to be performed on click here");		// what it does goes here
+				//world.die()???
+				//There's gotta be some FolderViewPanel??? 
+				JFrame frame = new JFrame();
+				
+				frame.setSize(Main.getWidth(), Main.getHeight());
+				frame.setTitle("Pandativity");
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setLayout(null);
+				
+				}
+			};
+		addActionListener(al); 
+	}
 
 }
--- src/NavigationPanel.java
+++ src/NavigationPanel.java
@@ -1,4 +1,23 @@
+import javax.swing.JPanel;
 
-public class NavigationPanel {
+public class NavigationPanel extends JPanel{
 
+	private int width;
+	private int height; 
+	
+	public NavigationPanel(int upperLeftX, int upperLeftY, int width, int height) {
+		super();
+		this.setBounds(upperLeftX, upperLeftY, width, height);
+		this.setLayout(null);
+		this.width = width;
+		this.height = height; 
+		
+		constructAll(); 
+	}
+	
+	public void constructAll() {
+		for(Folder folder:Main.getAllFolers()) {
+			
+		}
+	}
 }
