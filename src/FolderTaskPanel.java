4d0e78b2c81d09fd685f0d6f9d278de92203f727
localmgr <xiaj@rose-hulman.edu>
2021-05-10 20:26:45 -0400

    TODO: What to show on FolderTaskPanel (JLabel, JButton...?)

--- src/FolderTaskPanel.java
+++ src/FolderTaskPanel.java
@@ -4,18 +4,22 @@ public class FolderTaskPanel extends JPanel{
 	
 	private int width;
 	private int height; 
+	private Folder myFolder; 
 	
-	public FolderTaskPanel(int upperLeftX, int upperLeftY, int width, int height) {
+	public FolderTaskPanel(int upperLeftX, int upperLeftY, int width, int height, Folder folder) {
 		super();
 		this.setBounds(upperLeftX, upperLeftY, width, height);
 		this.setLayout(null);
 		this.width = width;
 		this.height = height; 
+		this.myFolder = folder; 
 		
 		constructAll(); 
 	}
 	
 	public void constructAll() {
-		//words
+		for(Task task:Main.getTasksByFolder(myFolder)) {
+			//TODO: What to show? 
+		}
 	}
 }
