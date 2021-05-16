a8556adef4b066552835ae633d1510234e54d967
localmgr <xiaj@rose-hulman.edu>
2021-05-15 22:48:34 -0400

    little changes

--- src/FolderLabelsPanel.java
+++ src/FolderLabelsPanel.java
@@ -5,21 +5,31 @@ import javax.swing.JPanel;
 
 public class FolderLabelsPanel extends JPanel{
 	
-	private ArrayList<Label> labels = new ArrayList<Label>();
+	private Folder folder; 
 	private int width;
 	private int height; 
+	private static ArrayList<LabelButton> allLabelButtons = new ArrayList<LabelButton>(); 
 
-	public FolderLabelsPanel(int upperLeftX, int upperLeftY, int width, int height) {
+	public FolderLabelsPanel(int upperLeftX, int upperLeftY, int width, int height, Folder folder) {
 		super();
 		this.setBounds(upperLeftX, upperLeftY, width, height);
 		this.setLayout(null);
 		this.width = width;
 		this.height = height; 
+		this.folder = folder; 
 		
 		constructAll(); 
 	}
 	
 	public void constructAll() {
-		//words
+		for(Label label:this.folder.getLabels()) {
+			
+		}
+	}
+	
+	public static void cleanAll() {
+		for(LabelButton i: allLabelButtons) {
+			i.unClick();
+		}
 	}
 }
--- src/LabelButton.java
+++ src/LabelButton.java
@@ -1,13 +1,13 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class LabelButton extends Button {
+public class LabelButton extends ToggleButton {
 	private Label label;
+	private boolean selected = false; 
 	
-	public LabelButton(Label label) { //? 
-		super(); 
+	public LabelButton(Label label, int upperLeftX, int upperLeftY, int width, int height) { //? 
+		super(label.getText()); 
 		this.label=label;
-		this.setText(label.getText());
 		this.createAction();
 	}
 	
@@ -15,11 +15,30 @@ public class LabelButton extends Button {
 		ActionListener al = new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				//System.out.println("Put actions to be performed on click here");		// what it does goes here
-				label.changeState();
-				changeColorState();
+				//----Steven Xia: using ToggleButton instead----
+//				label.changeState();
+//				changeColorState();
+				//---------
+				
+					if(selected) {
+						unClick(); 
+					}else {
+						FolderLabelsPanel.cleanAll();
+						displayTask(); 
+						setSelected(true); 
+						selected = true; 
+					}
 				}
 			};
 		addActionListener(al); 
 	}
 	
+	public void unClick() {
+		selected = false; 
+		this.setSelected(false);
+	}
+	
+	public void displayTask() {
+		
+	}
 }
