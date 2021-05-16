48163129e491af7aa87398383661f844acd08d67
Jesus Capo <jcapostem@gmail.com>
2021-05-16 19:55:34 -0400

    fixed checkbutton

--- src/CheckButton.java
+++ src/CheckButton.java
@@ -17,6 +17,7 @@ public class CheckButton extends JToggleButton{
 		protected int height; 
 		protected Color color;
 		private boolean toggle; 
+		final private CheckButton thisCheckButton;
 		// protected ??? link
 
 		// Getters, setters and variables for size and text are implemented by JButton.
@@ -28,6 +29,7 @@ public class CheckButton extends JToggleButton{
 			this.setText(text);
 			this.createAction();
 			color = new Color(255,255,255);
+			this.thisCheckButton=this;
 		}
 		
 		public CheckButton(String text, Task task, double upperLeftX, double upperLeftY, double width, double height) {
@@ -42,6 +44,7 @@ public class CheckButton extends JToggleButton{
 			color = new Color(255,255,255);
 			this.setLayout(null);
 			this.toggle = false; 
+			this.thisCheckButton=this;
 			//System.out.println("Check button created");
 		}
 
@@ -60,7 +63,7 @@ public class CheckButton extends JToggleButton{
 		public void setAppearance(/* appearance */) { // Appearance
 
 		}
-		private void changeState() { 
+		private void changeState() throws Exception { 
 			if (this.toggle == false) {
 				this.task.setCompleted(true);
 				System.out.println("changed to true");
@@ -75,7 +78,12 @@ public class CheckButton extends JToggleButton{
 			ActionListener al = new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
 					//System.out.println("Put actions to be preformed on click here");
-					changeState();
+					try {
+						thisCheckButton.changeState();
+					} catch (Exception e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
 					}
 				};
 				addActionListener(al); 
